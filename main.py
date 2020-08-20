cfa0abc2db19dddf8ece4703f984b16f5ce61f33
jjmacagnan <jlmacagnan@gmail.com>
2020-08-19 23:29:41 -0300

    save

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-input_file = open('texture_spectrum_CBIS-DDSM.csv', 'r')
-output_file = open('texture_spectrum_CBIS-DDSM.txt', 'w')
+input_file = open('Copy of daubechies.csv', 'r')
+output_file = open('Copy of daubechies.txt', 'w')
 # input_file.readline() # skip first line
 for line in input_file:
     output_file.write(" ".join(line.split()).replace(',', ' ') + '\n')
