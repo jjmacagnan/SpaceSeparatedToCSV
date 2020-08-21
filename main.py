34096644df452ca9160a52bbe454e9bbc6809163
JJMacagnan <jlmacagnan@gmail.com>
2020-08-20 21:26:18 -0300

    save

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-input_file = open('Copy of daubechies.csv', 'r')
-output_file = open('Copy of daubechies.txt', 'w')
+input_file = open('csv_flies/zernike.csv', 'r')
+output_file = open('txt_files/zernike.txt', 'w')
 # input_file.readline() # skip first line
 for line in input_file:
     output_file.write(" ".join(line.split()).replace(',', ' ') + '\n')
