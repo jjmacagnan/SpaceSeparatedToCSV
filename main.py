58ffb6006492cb96609d8af79170b21303d87d16
JJMacagnan <jlmacagnan@gmail.com>
2020-09-01 22:59:04 -0300

    convert all csv files to txt

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-input_file = open('csv_flies/zernike.csv', 'r')
-output_file = open('txt_files/zernike.txt', 'w')
-# input_file.readline() # skip first line
-for line in input_file:
-    output_file.write(" ".join(line.split()).replace(',', ' ') + '\n')
-input_file.close()
-output_file.close()
+import os
+
+csv_path = 'csv_files'
+txt_path = 'txt_files'
+
+files = os.listdir(csv_path)
+
+for filename in files:
+    print(filename)
+
+    input_file = open(csv_path + "/" + filename, 'r')
+
+    # remove .csv from filename
+    if filename.endswith('.csv'):
+        filename = filename[:-4]
+
+    output_file = open(txt_path + "/" + filename + '.txt', 'w')
+    # input_file.readline() # skip first line
+    for line in input_file:
+        output_file.write(" ".join(line.split()).replace(',', ' ') + '\n')
+    input_file.close()
+    output_file.close()
+
+
+
+
+
+
