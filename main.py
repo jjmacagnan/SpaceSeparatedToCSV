f26eeeb007e71f2f497223c81eda7b45221ea0f6
JJMacagnan <jlmacagnan@gmail.com>
2020-10-20 18:42:38 -0200

    save

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 csv_path = 'csv_files'
 txt_path = 'txt_files'
 
+# converts comma separated by single space
+
 files = os.listdir(csv_path)
 
 for filename in files:
@@ -20,6 +22,27 @@ for filename in files:
         output_file.write(" ".join(line.split()).replace(',', ' ') + '\n')
     input_file.close()
     output_file.close()
+
+
+# converts single spaces with a comma
+
+# files = os.listdir(txt_path)
+#
+# for filename in files:
+#     print(filename)
+#
+#     input_file = open(txt_path + "/" + filename, 'r')
+#
+#     # remove .csv from filename
+#     if filename.endswith('.txt'):
+#         filename = filename[:-4]
+#
+#     output_file = open(csv_path + "/" + filename + '.csv', 'w')
+#     # input_file.readline() # skip first line
+#     for line in input_file:
+#         output_file.write(" ".join(line.split()).replace(' ', ',') + '\n')
+#     input_file.close()
+#     output_file.close()
 
 
 
